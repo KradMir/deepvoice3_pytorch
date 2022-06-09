b06ebfec34d6b72b372d05f96d2c51630487deee
Тарасова Полина <ugugu25@yandex.ru>
2022-06-09 11:07:25 +0300

    4.0 - kernel size=3, bolshe sloyov v decoder

--- deepvoice3_pytorch/builder.py
+++ deepvoice3_pytorch/builder.py
@@ -43,7 +43,6 @@ def deepvoice3(n_vocab, embed_dim=256, mel_dim=80, linear_dim=513, r=4,
         # (channels, kernel_size, dilation)
         convolutions=[(h, k, 1), (h, k, 3), (h, k, 9), (h, k, 27),
                       (h, k, 1), (h, k, 3), (h, k, 9), (h, k, 27),
-                      (h, k, 1), (h, k, 3), (h, k, 9), (h, k, 27),
                       (h, k, 1), (h, k, 3)],
     )
 
@@ -54,6 +53,7 @@ def deepvoice3(n_vocab, embed_dim=256, mel_dim=80, linear_dim=513, r=4,
         dropout=dropout, max_positions=max_positions,
         preattention=[(h, k, 1), (h, k, 3)],
         convolutions=[(h, k, 1), (h, k, 3), (h, k, 9), (h, k, 27),
+                      (h, k, 1), (h, k, 3),
                       (h, k, 1)],
         attention=[True, False, False, False, True],
         force_monotonic_attention=force_monotonic_attention,
